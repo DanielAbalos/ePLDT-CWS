c2e2ea27e23e26a27387ee6a6ddcf17f9b07bc43
IT OJT <IT_OJT@EPLDT.COM>
2019-03-06 13:13:08 +0800

    initial commit

---
+++ src/controller/Login.java
@@ -0,0 +1,93 @@
+package controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.UserBean;
+
+@WebServlet("/login.html")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.sendRedirect("errorpage.html");
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		UserBean ub = new UserBean();
+		
+		ub.setUsername(request.getParameter("username"));
+		ub.setPassword(request.getParameter("password"));
+		
+		HttpSession session = request.getSession(false);
+		
+		if(session == null){
+			session = request.getSession();
+			session.setAttribute("user", ub.getUsername());
+			session.setMaxInactiveInterval(24*60*60);
+		
+		}else{
+			request.getRequestDispatcher("home.html").forward(request, response);
+		}
+		
+		System.out.println(session.getAttribute("user"));
+
+		if(validateUser(ub.getUsername(), ub.getPassword())){
+			request.getRequestDispatcher("home.html").forward(request, response);
+		
+		}else{
+			request.getRequestDispatcher("loginerror.html").forward(request, response);
+		}
+		
+	}
+	
+	private static boolean validateUser(String username, String password){
+		
+		String status = "false";
+		
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cws_db","root","");
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE username = '" + username + "'");
+
+			while(rs.next()){
+				System.out.println(rs.getString(6));
+				
+				if(rs.getString(6).equals(password)){
+					status = "true";
+				
+				}else{
+					status = "false";
+				}
+			}
+
+		}catch(SQLException sqle){
+			System.out.println("SQL Error in connectToUserDB - Login.java");
+			sqle.printStackTrace();
+		
+		}catch(ClassNotFoundException cnfe){
+			cnfe.printStackTrace();
+		}
+		
+		if(status.equals("true")){
+			return true;
+		}else{
+			return false;
+		}
+
+	}
+}
